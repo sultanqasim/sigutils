b32420a832fb338091f31816456d1e3c24624f07
Sultan Qasim Khan <sultanqasim@yandex.com>
2023-09-03 00:52:14 -0400

    lib: new parallel fft planning function
    Also hold a mutex while planning, since planning is not thread safe in
    fftw.

--- src/sigutils/lib.c
+++ src/sigutils/lib.c
@@ -22,10 +22,12 @@
 #define SU_LOG_LEVEL "lib"
 
 #include <sigutils/sigutils.h>
+#include <pthread.h>
 
 SUPRIVATE SUBOOL su_log_cr = SU_TRUE;
 SUPRIVATE SUBOOL su_measure_ffts = SU_FALSE;
 SUPRIVATE char *su_wisdom_file = NULL;
+SUPRIVATE pthread_mutex_t fft_plan_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 SUPRIVATE char
 su_log_severity_to_char(enum sigutils_log_severity sev)
@@ -124,6 +126,30 @@ int
 su_lib_fftw_strategy(void)
 {
   return su_measure_ffts ? FFTW_MEASURE : FFTW_ESTIMATE;
+}
+
+SU_FFTW(_plan)
+su_lib_plan_dft_1d(int n, SU_FFTW(_complex) *in, SU_FFTW(_complex) *out,
+        int sign, unsigned flags)
+{
+  SU_FFTW(_plan) plan;
+  int nthreads;
+
+  if (n < 32768)
+    nthreads = 1;
+  else if (n == 32768)
+    nthreads = 2;
+  else
+    nthreads = 4;
+
+  if (pthread_mutex_lock(&fft_plan_mutex) == -1)
+    return NULL;
+  SU_FFTW(_plan_with_nthreads)(nthreads);
+  plan = SU_FFTW(_plan_dft_1d)(n, in, out, sign, flags);
+  SU_FFTW(_plan_with_nthreads)(1);
+  pthread_mutex_unlock(&fft_plan_mutex);
+
+  return plan;
 }
 
 SUBOOL
